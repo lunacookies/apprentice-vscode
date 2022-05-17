5b324ef9d1ee20dd925bddc5e89fb4faad09a0bf
Luna Razzaghipour <aramisnoah@gmail.com>
2022-05-18 09:40:46 +1000

    Highlight comments

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ fn ui(t: &mut ThemeBuilder, p: &Palette) {
 
 fn editor(t: &mut ThemeBuilder, p: &Palette) {
     t.a([s("keyword")], p.light_blue);
+
+    t.a([s("comment")], p.light_grey);
 }
 
 struct Palette {
